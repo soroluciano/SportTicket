ab9141c3795e5745e583321c43c9f4618821dfd0
luciano soro <soro.luc@gmail.com>
2014-06-17 23:29:34 -0300

    some changes

--- grails-app/controllers/sporttickets/RankingController.groovy
+++ grails-app/controllers/sporttickets/RankingController.groovy
@@ -2,8 +2,8 @@ package sporttickets
 
 class RankingController {
 	def scaffold = true
-	static allowedMethods = [save: "POST",update:"POST",delete:"POST"]
-    def index() { 
-		redirect(action: "list", params:params)
-	}
+//	static allowedMethods = [save: "POST",update:"POST",delete:"POST"]
+//    def index() { 
+//		redirect(action: "list", params:params)
+//	}
 }
